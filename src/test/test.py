ea805d0fb15ea2297d907ec35af2c75cd621fba8
Beanpow <beanpow@gmail.com>
2022-10-16 22:01:48 +0800

    test: update vel based set pos

--- src/test/test.py
+++ src/test/test.py
@@ -8,6 +8,7 @@ from AxoController import AxoController  # noqa: E402
 def main():
     port = "COM3"
     axo_ctrl = AxoController(port=port, angle_telorance=[20, 60, 80, 5])
+    # axo_ctrl.close_receive_info()
     # axo_ctrl.enter_control_mode()
 
     # axo_ctrl.set_all_motors_pos_sync([10, 0, 10, 0])
@@ -15,7 +16,13 @@ def main():
     # axo_ctrl.set_all_motors_pos_sync([40, -30, 40, -30])
 
     # axo_ctrl.exit_control_mode()
-    time.sleep(10)
+    # time.sleep(10)
+    start_t = time.sleep(100)
+    # for i in range(15):
+    # axo_ctrl.change_control_mode("position")
+    # axo_ctrl.set_all_motors_pos_async([0, 0, 0, 0])
+
+    # print(f"change control mode time is {time.time() - start_t}")
     axo_ctrl.close_controller()
 
 
--- src/test/test_vel_based_set_pos.py
+++ src/test/test_vel_based_set_pos.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import time
 
 from simple_pid.PID import PID
@@ -9,23 +10,32 @@ from AxoController import AxoController  # noqa: E402
 
 def main():
     port = "com3"
-    axo = AxoController(port=port)
+    axo = AxoController(port=port, angle_telorance=[10, 50, 65, 3])
     axo.enter_control_mode()
+    axo.change_control_mode("position")
+    axo.set_all_motors_pos_sync([0, 0, 0, 0])
     axo.change_control_mode("velocity")
 
-    pid = PID(0, 0, 0, 0)
+    max_vel = 500
 
-    for i in range(100):
-        current_value = axo.get_leg_pos()[0]
-        output = pid(current_value)
-        assert output is not None
-        output = max(min(output, -100), 100)
+    pid = PID(60, 0, 1, 0)
 
-        axo.set_all_motors_vel([output, 0, 0, 0])
-        time.sleep(0.1)
+    try:
+        print("Start")
+        for i in range(2000):
+            current_value = axo.get_leg_pos()[2]
+            pid.setpoint = math.sin(i / 20) * 20 + 15
+            axo.target_pos = [0, 0, pid.setpoint, 0]
+            output = pid(current_value)
+            assert output is not None
+            output = min(max(output, -max_vel), max_vel)
+            print(f"control value is :{output}, current value is {current_value}, setpoint is {pid.setpoint}")
 
-    axo.exit_control_mode()
-    axo.close_controller()
+            axo.set_all_motors_vel([0, 0, output, 0])
+            time.sleep(0.01)
+    finally:
+        axo.exit_control_mode()
+        axo.close_controller()
 
 
 if __name__ == "__main__":
