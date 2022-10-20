1fd453f15209a8b44f111d2e69e8a2e4bd2991cd
Beanpow <beanpow@gmail.com>
2022-10-20 16:37:29 +0800

    feat: add spline interpolate

--- src/gait_gen/export_gait.py
+++ src/gait_gen/export_gait.py
@@ -10,6 +10,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from scipy import interpolate
 
 
 def main():
@@ -24,6 +25,24 @@ def main():
 
             gait = [float(line[i]) if i in [9, 4] else float(line[i]) * 180 / math.pi for i in [9, 12, 4, 7]]
             total_gait.append(gait)
+
+    total_gait = np.array(total_gait)
+    t = np.arange(len(total_gait)).reshape(-1, 1) / len(total_gait)
+
+    total_y_new = np.empty((len(total_gait) - 1, 4))
+    total_y_hat_new = np.empty((len(total_gait) - 1, 4))
+    for i in range(4):
+        tck = interpolate.splrep(t, total_gait[:, i], s=40, per=True)
+
+        x_new = np.arange(0, 1.02, 0.02)
+        y_new = interpolate.splev(x_new, tck, der=0)[:-1]
+        y_hat_new = interpolate.splev(x_new, tck, der=1)[:-1]
+        total_y_new[:, i] = y_new
+        total_y_hat_new[:, i] = y_hat_new
+
+    print(total_y_hat_new.shape, total_y_new.shape)
+    total_gait = np.vstack((total_y_new, total_y_hat_new))
+    print(total_gait.shape)
 
     with open("./final_gait.csv", "w") as f:
         for gait in total_gait:
