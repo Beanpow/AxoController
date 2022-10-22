4ddd857696eb3e984cbb7f48fbe9c6740a583122
Beanpow <beanpow@gmail.com>
2022-10-22 22:28:38 +0800

    feat: update trj format

--- src/SafetyController.py
+++ src/SafetyController.py
@@ -10,7 +10,6 @@
 
 import time
 from tqdm import tqdm
-import math
 from InfoPlottor import InfoPlottor
 from AxoController import AxoController
 from utils import load_trj
@@ -24,20 +23,21 @@ class SafetyController:
             self.info_plottor = InfoPlottor(axo_controller=self.axo_controller)
             self.info_plottor.open_plot_info()
 
-    def run_one_cycle(self, trajectory: list[list[float]], one_cycle_duration: float):
-        one_step_time = one_cycle_duration / len(trajectory)
-        for target_pos in trajectory:
+    def run_one_cycle(self, trajectory: tuple[list[list[float]], list[list[float]]], one_cycle_duration: float):
+        traj_pos, traj_vel = trajectory
+        one_step_time = one_cycle_duration / len(traj_pos)
+        for target_pos, target_vel in zip(traj_pos, traj_vel):
             start_time = time.time()
             if self.isPlot:
                 self.info_plottor.set_target_pos(target_pos)
 
-            self.axo_controller.set_all_motors_pos_vel_based(target_pos)
+            self.axo_controller.set_all_motors_pos_vel_based(target_pos, target_vel)
 
             assert time.time() - start_time <= one_step_time, "one step time is too short"
             print(one_step_time - (time.time() - start_time))
             time.sleep(one_step_time - (time.time() - start_time))
 
-    def run(self, trajectory: list[list[float]], duration: int):
+    def run(self, trajectory: tuple[list[list[float]], list[list[float]]], duration: int):
         self.axo_controller.enter_control_mode()
         time.sleep(4)  # wait for the robot reset to the initial position
 
@@ -45,7 +45,7 @@ class SafetyController:
         self.axo_controller.change_control_mode("velocity")
         self.axo_controller.set_all_motors_vel([0, 0, 0, 0])
         self.axo_controller.enter_control_mode(isChangeMode=False)
-        self.axo_controller.set_all_motors_pos_vel_based_sync(trajectory[0])
+        self.axo_controller.set_all_motors_pos_vel_based_sync(trajectory[0][0])
 
         start_time = time.time()
         last_time = start_time
@@ -73,8 +73,8 @@ if __name__ == "__main__":
     safety_controller = SafetyController()
     try:
         # traj = [[math.sin(i / 10 + math.pi / 2) * 15 + 10, -(math.sin(i / 10 + math.pi / 2) * 15 + 15), math.sin(i / 10) * 15 + 10, -(math.sin(i / 10) * 15 + 15)] for i in range(1000)]
-        traj = load_trj("./gait_gen/final_gait.csv")
-        safety_controller.run(trajectory=traj, duration=1800)
+        traj_pos, traj_vel = load_trj("./gait_gen/final_gait.csv")
+        safety_controller.run(trajectory=(traj_pos, traj_vel), duration=1800)
     except KeyboardInterrupt:
         print("[info] Keyboard interrupt")
     finally:
