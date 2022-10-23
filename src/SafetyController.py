7b70fae74dabcd1948f1e5e4e023ffb27d4ea28e
Beanpow <beanpow@gmail.com>
2022-10-23 23:01:31 +0800

    feat: add moment manager

--- src/SafetyController.py
+++ src/SafetyController.py
@@ -10,6 +10,9 @@
 
 import time
 from tqdm import tqdm
+from typing import Callable
+import numpy as np
+
 from InfoPlottor import InfoPlottor
 from AxoController import AxoController
 from MomentManager import MomentManager
@@ -17,7 +20,10 @@ from utils import load_trj
 
 
 class SafetyController:
-    def __init__(self, axo_port: str, moment_port: str, isPlot: bool = True) -> None:
+    def __init__(self, axo_port: str, moment_port: str, trajectory: tuple[list[list[float]], list[list[float]]], isPlot: bool = True) -> None:
+        self.trajectory = trajectory
+        self.one_cycle_time = 4  # unit: second
+
         self.moment_menager = MomentManager(port=moment_port)
         self.axo_controller = AxoController(port=axo_port)
         self.isPlot = isPlot
@@ -25,10 +31,60 @@ class SafetyController:
             self.info_plottor = InfoPlottor(axo_controller=self.axo_controller)
             self.info_plottor.open_plot_info()
 
-    def run_one_cycle(self, trajectory: tuple[list[list[float]], list[list[float]]], one_cycle_duration: float):
-        traj_pos, traj_vel = trajectory
-        one_step_time = one_cycle_duration / len(traj_pos)
-        for target_pos, target_vel in zip(traj_pos, traj_vel):
+        self.safe_moment_raw = []
+        self.safe_moment = None
+
+    def record_safe_info(self, cycle_num: int):
+        self.axo_controller.enter_control_mode()
+        time.sleep(4)
+        self.axo_controller.exit_control_mode()
+
+        self.axo_controller.change_control_mode("velocity")
+        self.axo_controller.set_all_motors_vel([0, 0, 0, 0])
+        self.axo_controller.enter_control_mode(isChangeMode=False)
+        self.axo_controller.set_all_motors_pos_vel_based_sync(self.trajectory[0][0])
+
+        with tqdm(total=cycle_num) as pbar:
+            for _ in range(cycle_num):
+                self.run_one_cycle(callback_func=self.record_callback)
+                pbar.update(1)
+
+        if self.axo_controller.in_control_mode:
+            self.axo_controller.exit_control_mode()
+        self.axo_controller.change_control_mode("position")
+
+    def record_callback(self, indx: int):
+        moments = self.moment_menager.get_all_moments()[0]
+        moments = np.append(indx, moments)
+
+        assert isinstance(self.safe_moment_raw, list)
+        self.safe_moment_raw.append(moments)
+
+    def process_raw_data(self):
+        self.safe_moment_raw = np.array(self.safe_moment_raw)
+        assert self.safe_moment_raw.shape[0] > 0
+
+        max_indx = np.max(self.safe_moment_raw[:, 0]) + 1
+        indx_set = [[] for i in range(max_indx)]
+        for i in range(self.safe_moment_raw.shape[0]):
+            indx_set[self.safe_moment_raw[i, 0]].append(i)
+
+        safe_moment = []
+        for indx in indx_set:
+            if len(indx) == 0:
+                print("[error]: exist empty moment in some indx")
+
+            raw_moment = self.safe_moment_raw[indx, 1:]  # type: ignore
+            mean = np.mean(raw_moment, axis=0)
+            std = np.std(raw_moment, axis=0)
+            safe_moment.append(np.hstack((mean, std)))
+
+        self.safe_moment = np.array(safe_moment)
+
+    def run_one_cycle(self, callback_func: Callable):
+        traj_pos, traj_vel = self.trajectory
+        one_step_time = self.one_cycle_time / len(traj_pos)
+        for idx, (target_pos, target_vel) in enumerate(zip(traj_pos, traj_vel)):
             start_time = time.time()
             if not self.axo_controller.in_control_mode:
                 break
@@ -36,12 +92,27 @@ class SafetyController:
             if self.isPlot:
                 self.info_plottor.set_target_pos(target_pos)
 
+            callback_func(idx)
+
             self.axo_controller.set_all_motors_pos_vel_based(target_pos, target_vel)
 
             assert time.time() - start_time <= one_step_time, "one step time is too short"
             time.sleep(one_step_time - (time.time() - start_time))
 
-    def run(self, trajectory: tuple[list[list[float]], list[list[float]]], duration: int):
+    def detect_callback(self, indx: int):
+        moments = self.moment_menager.get_all_moments()[0]
+        assert self.safe_moment is not None
+
+        factor = 3
+
+        low_limitation = self.safe_moment[indx, 0:4] - factor * self.safe_moment[indx, 4:8]
+        high_limitation = self.safe_moment[indx, 0:4] + factor * self.safe_moment[indx, 4:8]
+
+        if np.any(moments < low_limitation) or np.any(moments > high_limitation):
+            print(f"[error]: moment is not safe, indx: {indx}, moments: {moments}, low_limitation: {low_limitation}, high_limitation: {high_limitation}")
+            self.axo_controller.exit_control_mode()
+
+    def run_with_detection(self, cycle_num: int):
         self.axo_controller.enter_control_mode()
         time.sleep(4)  # wait for the robot reset to the initial position
 
@@ -49,22 +120,15 @@ class SafetyController:
         self.axo_controller.change_control_mode("velocity")
         self.axo_controller.set_all_motors_vel([0, 0, 0, 0])
         self.axo_controller.enter_control_mode(isChangeMode=False)
-        self.axo_controller.set_all_motors_pos_vel_based_sync(trajectory[0][0])
+        self.axo_controller.set_all_motors_pos_vel_based_sync(self.trajectory[0][0])
 
-        start_time = time.time()
-        last_time = start_time
-        with tqdm(total=duration) as pbar:
-            while True:
+        with tqdm(total=cycle_num) as pbar:
+            for _ in range(cycle_num):
                 if not self.axo_controller.in_control_mode:
                     break
 
-                self.run_one_cycle(trajectory, one_cycle_duration=4)
-
-                now_time = time.time()
-                if now_time - start_time > duration:
-                    break
-                pbar.update(now_time - last_time)
-                last_time = now_time
+                self.run_one_cycle(self.detect_callback)
+                pbar.update(1)
 
         if self.axo_controller.in_control_mode:
             self.axo_controller.exit_control_mode()
@@ -78,11 +142,14 @@ class SafetyController:
 
 
 if __name__ == "__main__":
-    safety_controller = SafetyController(moment_port="com5", axo_port="com3", isPlot=True)
+    traj = load_trj("./gait_gen/final_gait.csv")
+    safety_controller = SafetyController(moment_port="com5", axo_port="com3", trajectory=traj, isPlot=True)
     try:
         # traj = [[math.sin(i / 10 + math.pi / 2) * 15 + 10, -(math.sin(i / 10 + math.pi / 2) * 15 + 15), math.sin(i / 10) * 15 + 10, -(math.sin(i / 10) * 15 + 15)] for i in range(1000)]
-        traj_pos, traj_vel = load_trj("./gait_gen/final_gait.csv")
-        safety_controller.run(trajectory=(traj_pos, traj_vel), duration=1800)
+        # safety_controller.run(duration=1800)
+
+        safety_controller.record_safe_info(4)
+        safety_controller.run_with_detection(10)
     except KeyboardInterrupt:
         print("[info]: Keyboard interrupt")
     finally:
